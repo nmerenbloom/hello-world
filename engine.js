a0e6dcb711e72578ce892b5e1c1e77d1eac2d98c
nmerenbloom <nmerenbloom@gmail.com>
2020-05-21 14:32:56 -0400

    render img

--- engine.js
+++ engine.js
@@ -5,6 +5,11 @@ function onNavigate(arg) {
         case 'toNoah':
             window.location = 'noah.html'; 
     }
+}
 
-    
+
+function renderPic() {
+    const picRoot = $("#picRoot");
+    const pic = '<img src="headshot.jpg" alt="Noah Headshot">';
+    picRoot.append(pic);
 }
